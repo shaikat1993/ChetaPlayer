ae7c667e8b8b05ba99606c9fa60757d19c4af466
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2025-06-16 23:48:03 +0300

    Initial Commit

---
+++ ChetaPlayer/ChetaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChetaPlayerApp.swift
+//  ChetaPlayer
+//
+//  Created by Shaikat on 16.6.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChetaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChetaPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChetaPlayer
+//
+//  Created by Shaikat on 16.6.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
